a1922f033ea039530ae38904c8c15acbc3734e68
vitclarke279 <vlopatyuk@icloud.com>
2023-03-22 20:21:06 +0000

    Hook React App to FastApi Service

--- reddit-app/src/App.js
+++ reddit-app/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/')
+      .then(response => setMessage(response.data.Hello))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
+    <div className="RedditApp">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
