0f15f9097d740cf9b0c6f76f022a0c27e3a08ec6
vitclarke279 <vlopatyuk@icloud.com>
2023-03-23 18:39:23 +0000

    implement health check

--- api/main.py
+++ api/main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+
+tags_metadata = [
+    {
+        "name": "Statistics",
+        "description": "Statistical information on reddit threads",
+    },
+    {
+        "name": "Healthcheck"
+    }
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Reddit"}
+@app.get(
+        '/health',
+        description="Healthcheck endpoint",
+        status_code=status.HTTP_200_OK,
+        tags=["Healthcheck"],
+    )
+async def health_check():
+    return {'status': 'OK'}
