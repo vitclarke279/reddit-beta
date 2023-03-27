e92de2322a852ebc38b54b9312c2393e16ec69cc
vitclarke279 <vlopatyuk@icloud.com>
2023-03-27 22:16:31 +0100

    Modify allingment of input and button

--- reddit-app/src/App.js
+++ reddit-app/src/App.js
@@ -4,33 +4,40 @@ import { ImHappy2, ImSad2 } from 'react-icons/im';
 
 
 const App = () => {
-  const [subreddit, setSubreddit] = useState('');
-  const [subreddits, setSubreddits] = useState([]);
+    const [subreddit, setSubreddit] = useState('');
+    const [subreddits, setSubreddits] = useState([]);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await fetch(`http://localhost:8000/reddit-api/stats/subreddit-mood?subreddit=${subreddit}`);
-    const data = await response.json();
-    const moodIcon = data === 'positive' ? <ImHappy2 /> : <ImSad2 />;
-    setSubreddits([...subreddits, { title: subreddit, mood: moodIcon }]);
-    setSubreddit('');
-  };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await fetch(`http://localhost:8000/reddit-api/stats/subreddit-mood?subreddit=${subreddit}`);
+        const data = await response.json();
+        const moodIcon = data === 'positive' ? <ImHappy2 /> : <ImSad2 />;
+        setSubreddits([...subreddits, { title: subreddit, mood: moodIcon }]);
+        setSubreddit('');
+    };
 
   return (
     <div>
       <Container m={2}>
         <form onSubmit={handleSubmit}>
-            <Input value={subreddit} onChange={(event) => setSubreddit(event.target.value)} />
-            <Button type="submit">Add Subreddit</Button>
+            <HStack>
+                <Input
+                    placeholder='Find out the current mood of your favourite subreddit'
+                    value={subreddit}
+                    onChange={(event) => setSubreddit(event.target.value)}
+                />
+              <Button type="submit">Add Subreddit</Button>
+            </HStack>
+
         </form>
         <List>
             {subreddits.map((subreddit) => (
-            <HStack spacing={8}>
-                {subreddit.mood}
-                <ListItem key={subreddit.title}>
-                {subreddit.title}
-                </ListItem>
-            </HStack>
+                <HStack spacing={8}>
+                    {subreddit.mood}
+                    <ListItem key={subreddit.title}>
+                        {subreddit.title}
+                    </ListItem>
+                </HStack>
             ))}
         </List>
         </Container>
