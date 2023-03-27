5e024e997d1dc3514af069744a1b8dedc25238eb
vitclarke279 <vlopatyuk@icloud.com>
2023-03-27 15:26:17 +0100

    Add Mood Function Tests

--- api/tests/views/test_utils.py
+++ api/tests/views/test_utils.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, Mock, patch
 
-from core.views.utils import get_pushshift_data
+from core.views.utils import get_pushshift_data, _compound_score_to_mood_mapper, get_mood_from_text
 
 
-class TestUtils(TestCase):
+class TestGetPushshiftData(TestCase):
     @patch(
             'core.views.utils.requests.get',
             Mock(return_value=MagicMock(
@@ -21,6 +21,7 @@ class TestUtils(TestCase):
     )
     def test_get_pushshift_data_returns_successful_request(self):
         response = get_pushshift_data(data_type='comment')
+
         assert response == {
             "data": [
                 {"body": "Happy Friday!"},
@@ -39,3 +40,27 @@ class TestUtils(TestCase):
             get_pushshift_data(data_type='comment')
 
         assert 'Pushshift api is returning 400' == str(context.exception)
+
+
+class TestMoodFunctions(TestCase):
+    def test_compound_score_to_mood_accurately_returns_positive(self):
+        response = _compound_score_to_mood_mapper(0.8)
+
+        assert response == 'positive'
+
+    def test_compound_score_to_mood_accurately_returns_negative(self):
+        response = _compound_score_to_mood_mapper(-0.1)
+
+        assert response == 'negative'
+
+    def test_get_mood_from_text_identifies_negative_text(self):
+        negative_text_list = ["bad", "i hate this", "you are so terrible"]
+        mood = get_mood_from_text(text=negative_text_list)
+
+        assert mood == 'negative'
+
+    def test_get_mood_from_text_identifies_positive_text(self):
+        negative_text_list = ["you are so good", "this is amazing", "love you all"]
+        mood = get_mood_from_text(text=negative_text_list)
+
+        assert mood == 'positive'
