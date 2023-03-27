c376f9d7fbf3114475d1d99d0c892e4863f4a32f
vitclarke279 <vlopatyuk@icloud.com>
2023-03-27 12:00:36 +0100

    Restructure api app

--- api/main.py
+++ api/main.py
@@ -4,34 +4,36 @@ from fastapi.middleware.cors import CORSMiddleware
 from core.views import stats
 
 
-tags_metadata = [
-    {
-        "name": "Statistics",
-        "description": "Statistical information on reddit threads",
-    },
-    {
-        "name": "Healthcheck"
-    }
-]
+def create_app():
+    tags_metadata = [
+        {
+            "name": "Statistics",
+            "description": "Statistical information on reddit threads",
+        },
+        {
+            "name": "Healthcheck"
+        }
+    ]
 
-app = FastAPI(openapi_tags=tags_metadata)
+    app = FastAPI(openapi_tags=tags_metadata)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(stats.view)
-
-
-@app.get(
-        '/health',
-        description="Healthcheck endpoint",
-        status_code=status.HTTP_200_OK,
-        tags=["Healthcheck"],
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
-async def health_check():
-    return {'status': 'OK'}
+
+    app.include_router(stats.view)
+
+    @app.get(
+            '/health',
+            description="Healthcheck endpoint",
+            status_code=status.HTTP_200_OK,
+            tags=["Healthcheck"],
+        )
+    async def health_check():
+        return {'status': 'OK'}
+
+    return app
---
+++ api/tests/views/__init__.py
@@ -0,0 +1,11 @@
+# from unittest import TestCase
+
+# from fastapi.testclient import TestClient
+
+# from main import app
+
+
+# class ApiTestCase(TestCase):
+#     def setUp(self):
+#         super().setUp()
+#         self.client = TestClient(app)
